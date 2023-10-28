4eeed3bce305cc894e5be85722a953d48c724e1c
Matthew Souden <msouden@gmail.com>
2023-10-28 15:38:21 -0500

    deleting site

--- wp-content/plugins/svg-support/vendor/enshrined/svg-sanitize/tests/AllowedAttributesTest.php
+++
@@ -1,5 +0,0 @@
-<br>
-<b>Fatal error</b>:  Uncaught Error: Class &quot;PHPUnit\Framework\TestCase&quot; not found in /Users/msouden/Local Sites/msouden/app/public/wp-content/plugins/svg-support/vendor/enshrined/svg-sanitize/tests/AllowedAttributesTest.php:10
-Stack trace:
-#0 {main}
-  thrown in <b>/Users/msouden/Local Sites/msouden/app/public/wp-content/plugins/svg-support/vendor/enshrined/svg-sanitize/tests/AllowedAttributesTest.php</b> on line <b>10</b><br>
